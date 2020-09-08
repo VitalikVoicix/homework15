9e1645c3d347204ec24dc8ccd197f72ccc0abafa
VitalikVoicix <voicix1502@gmail.com>
2020-09-08 13:14:52 +0300

    Updates

---
+++ precache-manifest.b1aa9f624f11664faa94c85895a0a788.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130406942c9ac80af3782e52289ed521",
+    "url": "/homework15/index.html"
+  },
+  {
+    "revision": "4020a087970aa3bcc2bf",
+    "url": "/homework15/static/css/main.0c0bd8e6.chunk.css"
+  },
+  {
+    "revision": "ab432ecc64bf422df657",
+    "url": "/homework15/static/js/2.110e802f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework15/static/js/2.110e802f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4020a087970aa3bcc2bf",
+    "url": "/homework15/static/js/main.309d186d.chunk.js"
+  },
+  {
+    "revision": "054aee15546a89b08d75",
+    "url": "/homework15/static/js/runtime-main.1ed3dea1.js"
+  },
+  {
+    "revision": "99ab6d1d567b8c5b3e331e247d1147bf",
+    "url": "/homework15/static/media/background.99ab6d1d.jpeg"
+  },
+  {
+    "revision": "8d3e51c7c10bb2d84c72b4682fa605b0",
+    "url": "/homework15/static/media/comment.8d3e51c7.svg"
+  },
+  {
+    "revision": "c2e1643f330325fb8920055d0df3d36e",
+    "url": "/homework15/static/media/like.c2e1643f.svg"
+  },
+  {
+    "revision": "3fea1f0ef1a73387c75a2b1218f17ac9",
+    "url": "/homework15/static/media/redo.3fea1f0e.png"
+  },
+  {
+    "revision": "501c6325ded11fd509ea06f807af1107",
+    "url": "/homework15/static/media/repost.501c6325.svg"
+  },
+  {
+    "revision": "499187f3b67e6683709b237e095b7e31",
+    "url": "/homework15/static/media/share.499187f3.svg"
+  }
+]);
